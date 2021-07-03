00f0dbcefb3a1fb48ae8d2934a80810b82f26bc7
amordz72 <amordz72@gmail.com>
2021-07-03 20:40:46 +0100

    jquey

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import bootsrabCss from "./plugins/bootstrap-vue";
-import bootsrabJs from "./plugins/bootstrap-vue";
-import popper from "./plugins/popper-vue";
-
-createApp(App).use(store).
-use(router).
-use(bootsrabCss).
-use(bootsrabJs).
-mount("#app");
+import "./plugins/bootstrap-vue";
+// .use(bootsrabCss)import bootsrabCss from "./plugins/bootstrap-vue";
+createApp(App).use(store).use(router).mount("#app");
